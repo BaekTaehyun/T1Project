d10fd6970c3f88871b1eed1f84485a3bb86b125a
dxerr <july1981@naver.com>
2019-05-30 11:04:39 +0900

    [이범용] - 일부 소스 잘못 적용된거 되돌림 - e52264ee0ac1965f6f3a82049ae6e4e8acb77c7b

--- Source/GameService/UI/GsUIManager.h
+++ Source/GameService/UI/GsUIManager.h
@@ -23,62 +23,58 @@ public:
 
 	virtual void BeginDestroy() override;
 	virtual void Tick(float DeltaSeconds) override;
-
-	/** 위젯 띄우기 */
-	UFUNCTION(BlueprintCallable, Category = "GsUI")
-	void PushByKeyName(FName InKey, class UGsUIParameter* InParam = nullptr);
 	
-	/** 위젯 띄우기 */
+	// 테이블 키를 통해 위젯 띄우기
+	TWeakObjectPtr<UGsUIWidgetBase> PushByKeyName(FName InKey, class UGsUIParameter* InParam = nullptr);
+
+	// 위젯 띄우기
 	UFUNCTION(BlueprintCallable, Category = "GsUI")
 	void Push(TSubclassOf<UGsUIWidgetBase> InClass, class UGsUIParameter* InParam = nullptr);
 
-	/** 대상 위젯 제거 */
+	// 대상 위젯 제거
 	UFUNCTION(BlueprintCallable, Category = "GsUI")
 	void Pop(UGsUIWidgetBase* InWidget);
 
-	/** 가장 윗 스택을 반환. 현재 보고있는 UI라고 봐도 된다 */
-	UFUNCTION(BlueprintCallable, Category = "GsUI")
-	UGsUIWidgetBase* StackPeek(); // Current
+	// 안드로이드 백키 처리
+	void Back();
 
-	UFUNCTION(BlueprintCallable, Category = "GsUI")
-	UGsUIWidgetBase* GetCachedWidget(FString InPathName);	
-
-	//UFUNCTION(BlueprintCallable, Category = "GsUI") // TEST: 블루프린트에 노출시키지 않을 것
+	// 사용 중인 위젯 삭제. 캐시된 것은 지우지 않음
 	void RemoveAllStack();
+	void RemoveAllNoStack();
+	
+	// 캐시된 위젯까지 전부 지움
+	void RemoveAll();	
 
-	//UFUNCTION(BlueprintCallable, Category = "GsUI") // TEST: 블루프린트에 노출시키지 않을 것
-	void RemoveAllTray();
-
-	/** 캐싱한 정보까지 지운다 */
-	//UFUNCTION(BlueprintCallable, Category = "GsUI") // TEST: 블루프린트에 노출시키지 않을 것
-	void RemoveAll();
-
-	//UFUNCTION(BlueprintCallable, Category = "GsUI") // TEST:
-	//void TestForceGC();
-
-	/** UIPathTable 에서 해당 키의 Row를 찾아 WidgetClass를 리턴한다 */
+	// UIPathTable 에서 해당 키의 Row를 찾아 WidgetClass를 리턴
 	TSubclassOf<UGsUIWidgetBase> GetWidgetClass(FName InKey);
 
 protected:
+	UGsUIWidgetBase* PushInter(TSubclassOf<UGsUIWidgetBase> InClass, class UGsUIParameter* InParam = nullptr);
 	void PushStack(UGsUIWidgetBase* InWidget, UGsUIParameter* InParameters = nullptr);
-	void PushUnstack(UGsUIWidgetBase* InWidget, UGsUIParameter* InParameters = nullptr);
+	void PushNoStack(UGsUIWidgetBase* InWidget, UGsUIParameter* InParameters = nullptr);
 	void PopStack(UGsUIWidgetBase* InWidget);
-	void PopUnstack(UGsUIWidgetBase* InWidget);
+	void PopNoStack(UGsUIWidgetBase* InWidget);
 
 	struct FGsTableUIPath* GetTableRow(FName InKey);
 	void AddToViewport(UGsUIWidgetBase* InWidget);
+	UGsUIWidgetBase* CreateWidgetInter(TSubclassOf<UGsUIWidgetBase> InClass);
+
+	// 가장 윗 스택을 반환
+	TWeakObjectPtr<UGsUIWidgetBase> StackPeek();
 
 protected:
-	/** FName에 GetPathName 넣어서 관리 */
-	UPROPERTY()
-	TMap<FName, UGsUIWidgetBase*> CachedWidgets; // Key: PathName
-
-	UPROPERTY()
-	TArray<UGsUIWidgetBase*> StackedWidgets;
-
-	UPROPERTY()
-	TArray<UGsUIWidgetBase*> UnstackedWidgets;  // 인스턴스 목록 있으면 필요없을듯한데
-
+	// 위젯 클래스 패스를 가진 테이블을 가진다
 	UPROPERTY()
 	UDataTable* WidgetClassTable;
+
+	// 사용되고 있는 위젯(Stack, NoStack에 들어있는 위젯). 중복 사용을 허용.
+	UPROPERTY()
+	TArray<UGsUIWidgetBase*> UsingWidgets;
+	
+	// 한 개만 캐싱한다. 성능에 큰 문제 없으면 캐싱을 안해도 될 것 같다.
+	UPROPERTY()
+	TMap<FName, UGsUIWidgetBase*> CachedWidgets; // Key: PathName	
+		
+	TArray<TWeakObjectPtr<UGsUIWidgetBase>> Stack;
+	TArray<TWeakObjectPtr<UGsUIWidgetBase>> NoStack;
 };
